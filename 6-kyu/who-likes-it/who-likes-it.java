b52c746982c734d0e9c202702d5e9ca28a931f02
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2025-05-03 22:34:06 +0530

    Update solution - CodeHub

--- 6-kyu/who-likes-it/who-likes-it.java
+++ 6-kyu/who-likes-it/who-likes-it.java
@@ -1,18 +1,11 @@
 class Solution {
     public static String whoLikesIt(String... names) {
-      StringBuilder result = new StringBuilder();
-      if(names.length == 0){
-        result.append("no one likes this");
-      } else if (names.length == 1){
-        result.append(names[0] + " likes this");
-      } else if (names.length == 2){
-        result.append(names[0] + " and " + names[1] + " like this");
-      } else if (names.length == 3){
-        result.append(names[0] + ", " + names[1] + " and " + names[2] + " like this");
-      } else {
-        result.append(names[0] + ", " + names[1] + " and " + (names.length-2) +" others like this");
-      }
-        
-      return result.toString();
+        switch (names.length) {
+          case 0: return "no one likes this";
+          case 1: return String.format("%s likes this", names[0]);
+          case 2: return String.format("%s and %s like this", names[0], names[1]);
+          case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+          default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+        }
     }
 }
