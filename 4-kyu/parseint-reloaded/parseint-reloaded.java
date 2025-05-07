f72c467cf96419a118e78307be19ad300a43fbf3
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2025-05-08 00:21:06 +0530

    Add solution - CodeHub

---
+++ 4-kyu/parseint-reloaded/parseint-reloaded.java
@@ -0,0 +1,34 @@
+        wordToDigit.put("two", 2);
+        wordToDigit.put("three", 3);
+        wordToDigit.put("four", 4);
+        wordToDigit.put("five", 5);
+        wordToDigit.put("six", 6);
+        wordToDigit.put("seven", 7);
+        wordToDigit.put("eight", 8);
+        wordToDigit.put("nine", 9);
+        wordToDigit.put("ten", 10);
+        wordToDigit.put("eleven", 11);
+        wordToDigit.put("twelve", 12);
+        wordToDigit.put("thirteen", 13);
+        wordToDigit.put("fourteen", 14);
+        wordToDigit.put("fifteen", 15);
+        wordToDigit.put("sixteen", 16);
+        wordToDigit.put("seventeen", 17);
+        wordToDigit.put("eighteen", 18);
+        wordToDigit.put("nineteen", 19);
+        wordToDigit.put("twenty", 20);
+        wordToDigit.put("thirty", 30); 
+        wordToDigit.put("forty", 40);
+        wordToDigit.put("fifty", 50);
+        wordToDigit.put("sixty", 60);
+        wordToDigit.put("seventy", 70);
+        wordToDigit.put("eighty", 80);
+        wordToDigit.put("ninety", 90);
+​
+        // Initialize word-to-multiplier map
+        wordToMultiplier.put("hundred", 100);
+        wordToMultiplier.put("thousand", 1000);
+        wordToMultiplier.put("million", 1000000);
+        //wordToMultiplier.put("billion", 1000000000); //The maximum number, which must be supported is 1 million (inclusively)
+    }
+}
